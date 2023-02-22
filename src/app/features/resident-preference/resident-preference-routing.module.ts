fcf03a08699fa35ba9ff6372f06171fca099ab24
libhouse <lib.house.dev@gmail.com>
2023-02-22 12:53:38 -0300

    create header resident preferences

--- src/app/features/resident-preference/resident-preference-routing.module.ts
+++ src/app/features/resident-preference/resident-preference-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResidentPreferenceComponent } from './resident-preference/resident-preference.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ResidentPreferenceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { TransactionComponent } from './components/transaction/transaction.component';
 import { LoaderComponent } from './components/loader/loader.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component'
 
 
 
 @NgModule({
-  declarations: [TransactionComponent, LoaderComponent],
+  declarations: [TransactionComponent, LoaderComponent, NavBarComponent],
   imports: [
     CommonModule,
     MatIconModule,
